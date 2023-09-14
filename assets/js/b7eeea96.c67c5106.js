3e5bef48f445f8faf474d2b87ae89f7d85d2ad28
KnownSH <xanp101@gmail.com>
2023-09-13 20:21:36 -0700

    Built and published by Moonwave

---
+++ assets/js/b7eeea96.c67c5106.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[390],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/DataService","label":"\u200bData\u200bService"}]')}}]);
---
+++ assets/js/d25e2695.95ef769f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[619],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/KnownSH/universalguardians-lib/blob/master","baseUrl":"/universalguardians-lib/","classOrder":[],"apiCategories":[]}')}}]);
